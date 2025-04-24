8f2be4b20f4a2116f62f507b0486fb39998e4f09
ketanmakwana2906 <131157534+ketanmakwana2906@users.noreply.github.com>
2025-04-24 14:11:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import json
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Load environment variables
-with open("portfolio_env.json") as f:
-    creds_json = f.read()
-    os.environ["PORTFOLIO_PARAM"] = creds_json
+# with open("portfolio_env.json") as f:
+#     creds_json = f.read()
+#     os.environ["PORTFOLIO_PARAM"] = creds_json
 cf_confluence = json.loads(os.environ.get("PORTFOLIO_PARAM"))
 d360_api_key = cf_confluence["D360_API_KEY"]
 to_number_d360 = cf_confluence["TO_NUMBER_D360"]
