cd6179ed985ea7981076385bc8293a723387a02b
ketanmakwana2906 <131157534+ketanmakwana2906@users.noreply.github.com>
2025-05-07 12:43:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from portfolio_api import *
 import os
 import urllib3
 import json
+import schedule
 
 # Disable warnings for insecure requests
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Load environment variables
-# with open("portfolio_env.json") as f:
-#     creds_json = f.read()
-#     os.environ["PORTFOLIO_PARAM"] = creds_json
+with open("portfolio_env.json") as f:
+    creds_json = f.read()
+    os.environ["PORTFOLIO_PARAM"] = creds_json
 cf_confluence = json.loads(os.environ.get("PORTFOLIO_PARAM"))
 d360_api_key = cf_confluence["D360_API_KEY"]
 to_number_d360 = cf_confluence["TO_NUMBER_D360"]
@@ -30,9 +31,11 @@ def main():
         str += str_list[0]
         str_remain = str_list[1:]
         send_whatsapp_msg(str, str_remain, d360_api_key, to_number_d360)
+        send_mail(str, str_remain, "ketanmakwana9191@gmail.com")
         return "Portfolio sent!!", 200
     except Exception as e:
-        return f"An error occurred: {str(e)}", 500
+        print(e)
+        return f"An error occurred: {e}", 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
